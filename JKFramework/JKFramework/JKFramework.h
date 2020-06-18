95f8922bafda71b8ccb5dfc77fdba27059178d5c
jitendra kumar <jitendrakumar@jitendras-MacBook-Pro.local>
2020-06-18 10:38:55 +0530

    Inital commit

---
+++ JKFramework/JKFramework/JKFramework.h
@@ -0,0 +1,19 @@
+//
+//  JKFramework.h
+//  JKFramework
+//
+//  Created by jitendra kumar on 6/18/20.
+//  Copyright © 2020 jitendra kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JKFramework.
+FOUNDATION_EXPORT double JKFrameworkVersionNumber;
+
+//! Project version string for JKFramework.
+FOUNDATION_EXPORT const unsigned char JKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JKFramework/PublicHeader.h>
+
+
---
+++ JKFramework/JKFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  JKFramework
+//
+//  Created by jitendra kumar on 6/18/20.
+//  Copyright © 2020 jitendra kumar. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    public static func doSomething() -> String {
+        return "Service class"
+    }
+    
+}
